f80dfee3108aade52a8b0142379e55d70d1419fc
Matt Chan <mchan393@uwo.ca>
2020-01-10 09:37:11 -0500

    starting to refactor login but wanna merge everything first

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import Firebase from 'firebase';
+import UserInput from '../components/UserInput';
 
 class AuthScreen extends Component {
- render() {
-   return (
-     <View>
-       <Text>AuthScreen</Text>
-       <Text>AuthScreen</Text>
-       <Text>AuthScreen</Text>
-       <Text>AuthScreen</Text>
-       <Text>AuthScreen</Text>
-       <Text>AuthScreen</Text>
-     </View>
-   );
- } 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  doFirebaseLogin = () => {
+    // do firebase login
+    console.log('woof');
+    // Firebase.auth()
+    //   .signInWithEmailAndPassword(email, password)
+    //   .then(() => {
+    //     navigation.navigate('Landing', {
+    //       payload: email,
+    //       typeOfLogin: 'Log In'
+    //     });
+    //   });
+  };
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <View>
+        <Text>Login in to HomeShare</Text>
+        <UserInput 
+          term={email}
+          placeholder="Email"
+          onTermChange={newTerm => this.setState({ email: newTerm })}
+          onTermSubmit={() => {}}
+          secure={false}
+        />
+        <UserInput
+          secure
+          term={password}
+          secureTextEntry
+          placeholder="Password"
+          onTermChange={newTerm => this.setState({ password: newTerm })}
+          onTermSubmit={() => {}}
+        />
+        <Button
+          title="Log In"
+          onPress={this.doFirebaseLogin}
+        />
+      </View>
+    );
+  } 
 }
 
 export default AuthScreen;
