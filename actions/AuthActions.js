c5c8a16871f8af8ef0cd87ffa64e67534a04814b
Matt Chan <mchan393@uwo.ca>
2020-01-12 13:56:04 -0500

    cleanup auth actions

---
+++ actions/AuthActions.js
@@ -0,0 +1,30 @@
+import firebase from 'firebase';
+import { 
+  EMAIL_CHANGED, 
+  PASSWORD_CHANGED, 
+  LOGIN_USER_SUCCESS
+} from './types';
+
+export const emailChanged = (text) => {
+  return {
+    type: EMAIL_CHANGED,
+    payload: text
+  }
+};
+
+export const passwordChanged = (text) => {
+  return {
+    type: PASSWORD_CHANGED,
+    payload: text
+  }
+};
+
+export const loginUser = ({ email, password }) => {
+  // async req
+  return async (dispatch) => {
+    let user = await firebase.auth().signInWithEmailAndPassword(email, password);
+  
+    dispatch({ type: LOGIN_USER_SUCCESS, payload: user })
+  }
+}
+
