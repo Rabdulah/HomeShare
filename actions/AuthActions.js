9413f7583156e8f54701f09121bf8666cdbdb0cd
Matt Chan <mchan393@uwo.ca>
2020-01-12 16:09:22 -0500

    add prettier, eslint

--- actions/AuthActions.js
+++ actions/AuthActions.js
@@ -1,41 +1,39 @@
 import firebase from 'firebase';
-import { 
-  EMAIL_CHANGED, 
-  PASSWORD_CHANGED, 
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
   LOGIN_USER_SUCCESS,
   LOGIN_USER_FAIL,
-  LOGIN_USER
+  LOGIN_USER,
 } from './types';
 
-export const emailChanged = (text) => {
+export const emailChanged = text => {
   return {
     type: EMAIL_CHANGED,
-    payload: text
-  }
+    payload: text,
+  };
 };
 
-export const passwordChanged = (text) => {
+export const passwordChanged = text => {
   return {
     type: PASSWORD_CHANGED,
-    payload: text
-  }
+    payload: text,
+  };
 };
 
 export const loginUser = ({ email, password }) => {
   // async req
-  return async (dispatch) => {
+  return async dispatch => {
     // dispatch login_user for "user loading" part (starts the spinner)
     dispatch({ type: LOGIN_USER });
 
     try {
-      let user = await firebase.auth().signInWithEmailAndPassword(email, password);
+      const user = await firebase.auth().signInWithEmailAndPassword(email, password);
 
       dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
-      
     } catch (error) {
       console.log(error);
       dispatch({ type: LOGIN_USER_FAIL });
     }
-  }
-}
-
+  };
+};
