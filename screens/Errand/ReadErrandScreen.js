83c0559e9f18d8d05cf40a84428ff1ea2fd8a2ad
Matt Chan <mchan393@uwo.ca>
2020-03-12 18:58:15 -0400

    complete join / leave errand

--- screens/Errand/ReadErrandScreen.js
+++ screens/Errand/ReadErrandScreen.js
@@ -4,7 +4,10 @@ import { connect } from 'react-redux';
 import moment from 'moment';
 import { Ionicons } from '@expo/vector-icons';
 import { Layout, Text } from '@ui-kitten/components';
+import { Button } from 'react-native-elements';
+import firebase from 'firebase';
 import { DARK_BLUE } from '../../styles/colours';
+import { viewErrand } from '../../actions';
 
 const styles = StyleSheet.create({
   headerText: {
@@ -122,51 +125,197 @@ class ReadErrandScreen extends Component {
     return this.attendantNameArrayToString(attendantNames);
   };
 
+  joinErrand = async () => {
+    const {
+      currentErrand,
+      navigation,
+      user,
+      address,
+      email,
+      firstName,
+      lastName,
+      username
+    } = this.props;
+
+    const userRef = await firebase
+      .firestore()
+      .collection('users')
+      .doc(user);
+
+    // get user references
+    const existingAttendantsAsRef = await Promise.all(
+      currentErrand.attendants.map(async el => {
+        const userRef = await firebase
+          .firestore()
+          .collection('users')
+          .doc(el.id);
+
+        return {
+          userRef
+        };
+      })
+    );
+    const newAttendantsAsRef = [...existingAttendantsAsRef, { userRef }];
+    const newAttendantsForStore = [
+      ...currentErrand.attendants,
+      {
+        address, // REDUX STORE DOESNT HAVE ADDRESS, NEEDS TO BE PUT IN WHEN WE FETCH GROUPS OR W/E
+        email,
+        name: {
+          firstName,
+          lastName
+        },
+        username,
+        id: user
+      }
+    ];
+
+    const newCurrentErrandForStore = {
+      ...currentErrand,
+      attendants: newAttendantsForStore
+    };
+    firebase
+      .firestore()
+      .collection('errands')
+      .doc(currentErrand.id)
+      .update({ attendants: newAttendantsAsRef })
+      .then(() => {
+        viewErrand(newCurrentErrandForStore);
+        navigation.state.params.onNewCurrentErrand(newCurrentErrandForStore);
+        navigation.goBack();
+      });
+  };
+
+  leaveErrand = async () => {
+    const { currentErrand, navigation, user } = this.props;
+
+    currentErrand.attendants = currentErrand.attendants.filter(el => el.id !== user);
+
+    // get user references
+    const newAttendantsAsRef = await Promise.all(
+      currentErrand.attendants.map(async el => {
+        const userRef = await firebase
+          .firestore()
+          .collection('users')
+          .doc(el.id);
+
+        return {
+          userRef
+        };
+      })
+    );
+
+    const newCurrentErrandForStore = {
+      ...currentErrand
+    };
+    firebase
+      .firestore()
+      .collection('errands')
+      .doc(currentErrand.id)
+      .update({ attendants: newAttendantsAsRef })
+      .then(() => {
+        viewErrand(newCurrentErrandForStore);
+        navigation.state.params.onNewCurrentErrand(newCurrentErrandForStore);
+        navigation.goBack();
+      });
+  };
+
+  onButtonPress = async () => {
+    if (this.isUserAttendingErrand()) {
+      // leave errand
+      this.leaveErrand();
+    } else {
+      // join errand
+      this.joinErrand();
+    }
+  };
+
+  onSave = async () => {
+    const { name, description, date } = this.state;
+    const { user, group } = this.props;
+
+    const updatedFields = {
+      name,
+      description,
+      date
+    };
+
+    this.updateErrand(updatedFields);
+  };
+
+  isUserAttendingErrand = () => {
+    const { currentErrand, user } = this.props;
+    const isUserAttending = currentErrand.attendants.find(el => el.id === user);
+
+    return !!isUserAttending;
+  };
+
+  renderButtonText = () => {
+    return this.isUserAttendingErrand() ? 'Leave Errand' : 'Join Errand';
+  };
+
   render() {
     const { name, date, owner, description } = this.props.currentErrand;
     return (
-      <ScrollView style={{ padding: 16 }}>
-        <Text style={{ fontSize: 25, lineHeight: 25, fontWeight: '700' }}>{name}</Text>
-        <Layout style={{ marginVertical: 20 }}>
-          <Text>{moment.unix(date).format('dddd, MMM d, YYYY')}</Text>
-          <Text>{moment.unix(date).format('h:mm A')}</Text>
-        </Layout>
+      <ScrollView
+        style={{ padding: 16 }}
+        contentContainerStyle={{
+          flexDirection: 'column',
+          flex: 1,
+          justifyContent: 'space-between'
+        }}
+      >
         <Layout>
-          <Layout
-            style={[
-              {
-                borderTopWidth: 1,
-                borderTopColor: '#dbdbdb'
-              },
-              styles.rowItem
-            ]}
-          >
-            <Text style={styles.text}>Errand event owner:</Text>
-            <Text style={styles.rowItemRight}>
-              {owner.name.firstName} {owner.name.lastName}
-            </Text>
+          <Text style={{ fontSize: 25, lineHeight: 25, fontWeight: '700' }}>{name}</Text>
+          <Layout style={{ marginVertical: 20 }}>
+            <Text>{moment.unix(date).format('dddd, MMM d, YYYY')}</Text>
+            <Text>{moment.unix(date).format('h:mm A')}</Text>
           </Layout>
-          <Layout style={styles.rowItem}>
-            <Text style={styles.text}>Roommates attending:</Text>
-            <Text style={styles.rowItemRight}>{this.renderAttendeeNames()}</Text>
-          </Layout>
-          <Layout
-            style={{
-              paddingVertical: 10
-            }}
-          >
-            <Text style={styles.text}>Roommates attending:</Text>
-            <Text style={styles.rowItemRight}>{description}</Text>
+          <Layout>
+            <Layout
+              style={[
+                {
+                  borderTopWidth: 1,
+                  borderTopColor: '#dbdbdb'
+                },
+                styles.rowItem
+              ]}
+            >
+              <Text style={styles.text}>Errand event owner:</Text>
+              <Text style={styles.rowItemRight}>
+                {owner.name.firstName} {owner.name.lastName}
+              </Text>
+            </Layout>
+            <Layout style={styles.rowItem}>
+              <Text style={styles.text}>Roommates attending:</Text>
+              <Text style={styles.rowItemRight}>{this.renderAttendeeNames()}</Text>
+            </Layout>
+            <Layout
+              style={{
+                paddingVertical: 10
+              }}
+            >
+              <Text style={styles.text}>Roommates attending:</Text>
+              <Text style={styles.rowItemRight}>{description}</Text>
+            </Layout>
           </Layout>
         </Layout>
+        <Button
+          title={this.renderButtonText()}
+          type="outline"
+          titleStyle={{ color: DARK_BLUE }}
+          buttonStyle={{ borderColor: DARK_BLUE }}
+          onPress={this.onButtonPress}
+        />
       </ScrollView>
     );
   }
 }
 
-const mapStateToProps = ({ errand }) => {
+const mapStateToProps = ({ errand, auth }) => {
   const { currentErrand } = errand;
+  const { user, username, firstName, lastName, email } = auth;
 
-  return { currentErrand };
+  return { currentErrand, user, username, firstName, lastName, email };
 };
 export default connect(mapStateToProps, null)(ReadErrandScreen);
